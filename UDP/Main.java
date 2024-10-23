3e50de02160db7513234dbe3714a77cee36585d6
Lucumerry <145458440+Arlausea@users.noreply.github.com>
2024-10-23 14:02:57 +0200

    Definition of UDPServer

---
+++ UDP/Main.java
@@ -0,0 +1,8 @@
+package UDP;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ UDP/UDPServer.java
@@ -0,0 +1,17 @@
+package UDP;
+
+public class UDPServer {
+
+    private int port;
+
+    // Constructor with one argument
+    public UDPServer(int listening_port){
+
+    }
+
+
+    // Default constructor
+    public UDPServer(){
+        this.port = 65365;
+    }
+}
