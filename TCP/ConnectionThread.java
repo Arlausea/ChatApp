9b00df71d6f2395226ac5e8093a42222175edc82
Arlosea <arthur.laumy@ensea.fr>
2024-12-04 16:24:37 +0100

    Update JavaDocs

--- TCP/ConnectionThread.java
+++ TCP/ConnectionThread.java
@@ -5,17 +5,35 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A Thread implementation for managing threads with multiple clients.
+ * @see TCPMultipleServer
+ */
 public class ConnectionThread extends Thread {
 
     private final Socket socket;
 //    private List<Socket> clients = new ArrayList<>();
 
+
+    /**
+     * Constructs a ConnectionThread with a given client socket.
+     */
     public ConnectionThread(Socket clientSocket) {
         this.socket = clientSocket;
     }
 
 
-
+    /**
+     * The run method of ConnectionThread. Directly launched at creation of the object.
+     * <p>
+     *
+     * Opens a socket and waits for a connection. When a client start a connection,
+     * then the server start a thread with the client socket to receive and send back messages.
+     * The connection is finished if the client ends its process (with exit or CTRL+D input)
+     * </p>
+     *
+     * @throws IOException if an error occurs while creating threads.
+     */
     @Override
     public void run() {
         while (true) {
@@ -32,7 +50,7 @@ public class ConnectionThread extends Thread {
                 String clientMessage = in.readLine();
                 System.out.println("Received message from client: " + clientMessage);
 
-                // Answer to one thread
+                // Response  to one thread
                 out.println("Echo : " + clientMessage);
 
 
